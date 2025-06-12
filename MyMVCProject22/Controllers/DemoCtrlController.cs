3c931dceeaf28c7e7e67da31f8919e13647c729e
sneha_gharat <42944127+snehagharat@users.noreply.github.com>
2025-06-12 15:39:24 +0530

    Added Login ctrl

---
+++ MyMVCProject22/Controllers/DemoCtrlController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCProject22.Controllers
+{
+    public class DemoCtrlController : Controller
+    {
+        // GET: DemoCtrl
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyMVCProject22/Controllers/LoginCtrlController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCProject22.Controllers
+{
+    public class LoginCtrlController : Controller
+    {
+        // GET: LoginCtrl
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
